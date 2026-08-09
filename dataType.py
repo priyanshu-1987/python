b691db5aaa14f485e689fa92936689dcf57c0d12
priyanshu-1987 <atulkumarg1987@gmail.com>
2026-08-09 16:49:16 +0530

    data types in python

---
+++ dataType.py
@@ -0,0 +1,13 @@
+a=12
+b=27.2
+c=35/7
+d=64j
+e=a+b+c
+st="hello1234@#$"
+print(type(a)) #int 
+print(type(b)) #float
+print(type(c))  #float
+print(type(d)) #complex
+print(type(st))#string
+print(e) #12 + 27.2 + 5.0 = 44.2
+print(type(e)) #float
