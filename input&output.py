934b86e14fbe23373fd017c93b2e39c113357a7e
priyanshu-1987 <atulkumarg1987@gmail.com>
2026-08-09 17:30:34 +0530

    input output

---
+++ input&output.py
@@ -0,0 +1,10 @@
+name="Priyanshu"
+age=23
+print(name,age)#Priyanshu 23
+print("hello my name is ",name,"and my age is ",age) #hello my name is  Priyanshu and my age is  23
+#using formatted string
+print(f"hello my name is {name} and my age is {age}") #hello my name is Priyanshu and my age is 23
+
+name1=input("Enter your name: ")
+age1=int(input("Enter your age: "))
+print(f"hello my name is {name1} and my age is {age1}") #hello my name is name1 and my age is age1
